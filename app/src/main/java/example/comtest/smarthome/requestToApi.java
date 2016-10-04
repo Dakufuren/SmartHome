1a046189aeab126a96d2dd5dad9a75c39926f99c
Axel Malmberg <axel_malmberg@hotmail.com>
2016-10-04 14:19:35 +0200

    Updated requestToApi class: changed to JSONobjectRequest instead of StringRequest and it is now working with the API as intended

--- app/src/main/java/example/comtest/smarthome/requestToApi.java
+++ app/src/main/java/example/comtest/smarthome/requestToApi.java
@@ -1,14 +1,20 @@
 package example.comtest.smarthome;
 
 import android.content.Context;
+import android.util.Log;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -21,6 +27,7 @@ public class requestToApi {
     String response;
     private RequestQueue requestQueue;
     private StringRequest request;
+    private JsonObjectRequest request2;
     private static String URL = "http://smarthomeinterface.azurewebsites.net/home/3";
     Context context;
 
@@ -32,36 +39,51 @@ public class requestToApi {
     public String postToServer(final String commandId, final String sensorId, final String userId){
         requestQueue = Volley.newRequestQueue(context);
 
-        request = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                //ack will be shown here
-                System.out.println("i was here");
+        JSONObject infoSent = new JSONObject();
+        try {
+            infoSent.put("commandId", commandId);
+            infoSent.put("sensorId", sensorId);
+            infoSent.put("userId", userId);
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+
+        request2 = new JsonObjectRequest(
+                Request.Method.POST,URL, infoSent,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        System.out.println("response here");
+                        try {
+                            System.out.println(response.getString("message"));
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
 
 
-            }
-        }, new Response.ErrorListener() {
+                    }
+                }, new Response.ErrorListener() {
+
             @Override
             public void onErrorResponse(VolleyError error) {
-                System.out.println("hej");
+                VolleyLog.d("Error: " );
             }
         }) {
 
+            /**
+             * Passing some request headers
+             */
             @Override
-            protected Map<String, String> getParams() throws AuthFailureError {
-                //hashmap represents the input that is going to be sent
-                HashMap<String, String> hashMap = new HashMap<String, String>();
-                hashMap.put("commandId:", commandId);
-                hashMap.put("sensorId:", sensorId);
-                hashMap.put("userId:", userId);
-                System.out.println(hashMap);
-
-                return hashMap;
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<String, String>();
+                headers.put("Content-Type", "application/json; charset=utf-8");
+                return headers;
             }
-
         };
+
         System.out.println("sending request");
-        requestQueue.add(request);
+        requestQueue.add(request2);
 
         return response;
     }
