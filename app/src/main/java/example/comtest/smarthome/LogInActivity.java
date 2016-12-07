8bc7ccfe72b82ae4842f66caeabef173e71bcacd
Sweetpink <starscream99@gmail.com>
2016-12-07 14:07:54 +0100

    .

---
+++ app/src/main/java/example/comtest/smarthome/LogInActivity.java
@@ -0,0 +1,17 @@
+package example.comtest.smarthome;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Sweetpink on 2016-11-27.
+ */
+
+public class LogInActivity  extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity);
+    }
+}
